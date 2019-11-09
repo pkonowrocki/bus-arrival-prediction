afc4271cb03741faf4c91a1d7406e3ebf01e0ea5
tpalayda <tpalayda@github.com>
2019-11-09 17:00:43 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+
+if __name__ == "__main__":
+    path = "/home/tpalayda/Desktop/studying/ppd/project/2018-05-26/part-0-0"
+
+    #TODO add specific dtypes to get rid of the warning
+    #dtype={"versionID": numpy.uint64}
+    data_names = ["versionID", "line", "brigade", "time", "lon", "lat", "rawLon", \
+                  "rawLat", "status", "delay", "delayAtStop", "plannedLeaveTime", \
+                  "nearestStop", "nearestStopDistance", "nearestStopLon", \
+                  "nearestStopLat", "previousStop", "previousStopLon", \
+                  "previousStopLat", "previousStopDistance", "previousStopArrivalTime", \
+                  "previousStopLeaveTime", "nextStop", "nextStopLon", "nextStopLat", \
+                  "nextStopDistance", "nextStopTimetableVisitTime", "courseID", \
+                  "courseDirection", "timetableID", "timetableStatus", "receivedTime", \
+                  "processingFinishedTime", "onWayToDepot", "overlapsWithNextBrigade", \
+                  "overlapsWithNextBrigadeStopLineBrigade", "atStop", "speed", "oldDelay" \
+                  "serverID", "delayAtStopStopSequence", "previousStopStopSequence", \
+                  "nextStopStopSequence", "delayAtStopStopID", "previousStopStopID", \
+                  "nextStopStopID", "coursDirectionStopStopID", "partition"] 
+
+    data = pd.read_csv(path, sep=';', header=None, names=data_names)
+    print(data.iloc[0])
+
