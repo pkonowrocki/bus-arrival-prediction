9cee87bd23a2d235a7ddce0464e10b8b4fb64b40
Arkadiusz Ryszewski <arkadiusz.ryszewski@sollers.eu>
2019-11-10 13:19:15 +0100

    Cleanup

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ import numpy as np
 
 def main():
     path = "data/2018-05-21/part-0-0"
+    data = readData(path)
+    showRows(data, amount=10)
+    showRowDetails(data, i=0)
 
+def readData(path):
     #TODO add specific dtypes to get rid of the warning
     #dtype={"versionID": numpy.uint64}
-    col_idx = dict.fromkeys(["versionID", "line", "brigade", "time", "lon", "lat", "rawLon", \
+    columnNames = ["versionID", "line", "brigade", "time", "lon", "lat", "rawLon", \
                   "rawLat", "status", "delay", "delayAtStop", "plannedLeaveTime", \
                   "nearestStop", "nearestStopDistance", "nearestStopLon", \
                   "nearestStopLat", "previousStop", "previousStopLon", \
@@ -18,23 +22,19 @@ def main():
                   "overlapsWithNextBrigadeStopLineBrigade", "atStop", "speed", "oldDelay" \
                   "serverID", "delayAtStopStopSequence", "previousStopStopSequence", \
                   "nextStopStopSequence", "delayAtStopStopID", "previousStopStopID", \
-                  "nextStopStopID", "coursDirectionStopStopID", "partition"])
-    i = 0
-    for key in col_idx.keys():
-        col_idx[key] = i
-        i += 1
+                  "nextStopStopID", "coursDirectionStopStopID", "partition"]
+    columnIndices = range(len(columnNames))
+    col_idx = dict(zip(columnNames, columnIndices))
+
     indexesOfDateColumns = [col_idx['time'], col_idx['receivedTime'], col_idx['processingFinishedTime']]
-    
     data = pd.read_csv(path, sep=';', header=None, names=col_idx.keys(), \
         parse_dates = indexesOfDateColumns)
 
     data = data.sort_values(by=['line', 'brigade', 'time'], ascending=True)
-    
-    print(f'Data set size: {data.size}. First {amount} rows of data:\n')
-    showRows(data, amount=10)
-    showRowDetails(data, i=0)
-    
+    return data
+
 def showRows(data, amount):
+    print(f'Data set size: {data.size}. First {amount} rows of data:\n')
     i = 0
     for ix, entry in data.iterrows():
         cols = ['time', 'line', 'brigade']
