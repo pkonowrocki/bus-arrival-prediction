4a3e263f5d87b93ed516f95781f5e4ac05d84730
Arkadiusz Ryszewski <arkadiusz.ryszewski@sollers.eu>
2019-12-07 19:51:25 +0100

    add missing global_path

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from utils import *
 
+global_path = 'data'
+
 def main():
     # Read single file
     #data = parse_file(global_path + '/2018-05-26/part-0-0')
 
     # Read all files in one directory
-    #parse_folder(global_path + '/2018-05-26')
+    parse_folder(global_path + '/2018-05-21')
 
     # show_rows(data, amount=10)
     # show_row_details(data, i=0)
 
     # filenames is hashmap in form of -> line (string) : list (list of strings)
-    filenames = traverse_directory(global_path + "/lines")
+    #filenames = traverse_directory(global_path + "/lines")
 
 if __name__ == "__main__":
     main()
