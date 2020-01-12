3315b0698a5dab9fa3c8fd5a9a57c3eee0ff8621
tpalayda <19340862+tpalayda@users.noreply.github.com>
2020-01-12 15:25:48 +0100

    cleaned learning process

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from utils import *
-#from ml import *
+from ml import *
 
 global_path = 'data'
 
@@ -10,12 +10,14 @@ def main():
     # Read all files in one directory
     #create_single_file(global_path + '/2018-05-21')
     #data = parse_folder(global_path + '/2018-05-21')
-    parse_folder_PCA(global_path + '/2018-05-21')
+    #parse_folder_PCA(global_path + '/2018-05-21')
+    #parse_folder_PCA(global_path + '/2018-05-26')
 
     # show_rows(data, amount=10)
     # show_row_details(data, i=0)
 
     # filenames is hashmap in form of -> line (string) : list (list of strings)
-
+    #learn()
+    
 if __name__ == "__main__":
     main()
